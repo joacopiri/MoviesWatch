cbcadd574b4bbecf7788d23c506747117f00048b
joacopiri <joaquinpirillo5102007@gmail.com>
2026-03-04 11:38:45 -0300

    nueva caracteristica: ahora es posible obtener informacion atravez del titulo de una pelicula

--- main.py
+++ main.py
@@ -8,15 +8,21 @@ app = FastAPI()
 
 @app.get("/ok")
 def read_root():
+    if not "APIKEY_OMDBAPI" in os.environ.keys():
+        return {"status": "nokey"}
     return {"status": "ok"}
 
 
 @app.get("/movies-test")
 def obtener_peliculas():
     r = requests.get(f"http://www.omdbapi.com/?apikey={os.environ.get("APIKEY_OMDBAPI")}&t=titanic")
-    print(r)
     return r.json()
 
 @app.get("/movies")
 def movies():
     return peliculas
+
+@app.get("/movies/{pelicula}")
+def movies_by_id(pelicula:str):
+    r_id = requests.get(f"http://www.omdbapi.com/?apikey={os.environ.get("APIKEY_OMDBAPI")}&t={pelicula}")
+    return r_id.json()
